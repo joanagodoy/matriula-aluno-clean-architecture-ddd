fc690dccb3a490d8940dcd10e7a31c9f840c95a8
joanagodoy <joanapereiragodoy@gmail.com>
2022-11-20 12:06:47 -0300

    Refatora teste unitário

--- src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
+++ src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
@@ -1,21 +1,40 @@
 package br.com.alura.escola.dominio.aluno;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AlunoTest {
+class AlunoTest {
 
-    @Test
-    public void deveriaDarExcecaoQuandoAdicionarTresTelefoes(){
-        Aluno aluno = new Aluno(new CPF("123.456.789-00"),
+    private Aluno aluno;
+
+    @BeforeEach
+    void befofeEach(){
+        this.aluno =  new Aluno(new CPF("123.456.789-00"),
                 "Fulano",
                 new Email("fulano@email.com"));
+    }
 
+    @Test
+    void deveriaPermitirAdicionarUmTelefone(){
+        aluno.adicionarTelefone("11", "32676595");
+        Assertions.assertEquals(1, aluno.getTelefones().size());
+    }
+
+    @Test
+    void deveriaPermitirAdicionarDoisTelefones(){
         aluno.adicionarTelefone("11", "32676595");
         aluno.adicionarTelefone("11", "32676595");
-        Assertions.assertThrows(
-                IllegalArgumentException.class,
-                () -> aluno.adicionarTelefone("11", "32676595"));
+        Assertions.assertEquals(2, aluno.getTelefones().size());
+    }
+
+    @Test
+    void naoDeveriaPermitirAdicionarTresTelefones(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            aluno.adicionarTelefone("11", "32676595");
+            aluno.adicionarTelefone("11", "32676595");
+            aluno.adicionarTelefone("11", "32676595");
+        });
     }
 
 }
